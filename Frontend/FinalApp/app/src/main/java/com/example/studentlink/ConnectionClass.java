a5e260a1002ae0e45954008089ada8b7fe45f84b
Devon Kooker <dckooker@iastate.edu]>
2020-09-17 15:58:59 -0500

    request update

---
+++ Frontend/FinalApp/app/src/main/java/com/example/studentlink/ConnectionClass.java
@@ -0,0 +1,101 @@
+package com.example.studentlink;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
+public class ConnectionClass extends AppCompatActivity {
+
+    private Context context;
+    public ConnectionClass(){};
+    String serverResponse = "";
+    IResult result;
+
+
+
+    //asks for any data coming to app
+    public void getRequest(String database, Context context)  {
+        this.context = context;
+
+
+        //Request Queue
+        RequestQueue RequestQueue = Volley.newRequestQueue(context);
+        String url = "https://b9bd0a6c-31b5-4e2a-8803-c10a85855393.mock.pstmn.io/" + database;
+
+        JsonObjectRequest ObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        RequestQueue.add(ObjRequest);
+        RequestQueue.start();
+    }
+
+    //sends data to database
+    public void putRequest(String data){
+
+        //our api's url
+        String url = "https://localhost:5001/test";
+
+        //start a queue for requests for our api
+        RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
+
+        //create a request object for our api asking for a json object to be returned
+        JsonObjectRequest ExampleRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //when our api responds then we do somthing with the data
+                Toast toast = Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT);
+            }
+
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+            }
+        });
+        //add the request object to the queue of requests
+        ExampleRequestQueue.add(ExampleRequest);
+    }
+
+    public void setResponse(String string){
+        serverResponse = string;
+    }
+
+    public String getResponse(){
+        return serverResponse;
+    }
+
+    public interface IResult {
+        public void notifySuccess(String response);
+
+        public void notifyError(VolleyError error);
+    }
+
+
+}
--- Frontend/FinalApp/app/src/main/java/com/example/studentlink/ui/profile/ProfileFragment.java
+++ Frontend/FinalApp/app/src/main/java/com/example/studentlink/ui/profile/ProfileFragment.java
@@ -1,24 +1,54 @@
 package com.example.studentlink.ui.profile;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.studentlink.ConnectionClass;
 import com.example.studentlink.R;
+
+import org.json.JSONObject;
 
 public class ProfileFragment  extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.profile_layout, container, false);
 
+        ConnectionClass connection = new ConnectionClass();
+
         TextView ProfileText = root.findViewById(R.id.ProfileText);
+        TextView requestData = root.findViewById(R.id.requestData);
+        Button RequestButton = root.findViewById(R.id.RequestButton);
 
         ProfileText.setText("Profile Page Stuff");
 
+        RequestButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                connection.getRequest("name_test", getContext(), new ConnectionClass.CustomListener(){
+                    @Override
+                    public void onVolleyResponse(String response) {
+                        requestData.setText(connection.getResponse());
+                    }
+                });
+
+            }
+        });
+
         return root;
     }
+
+
 }
