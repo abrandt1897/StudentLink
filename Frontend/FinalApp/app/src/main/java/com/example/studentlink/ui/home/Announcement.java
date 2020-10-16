fd3db5c6525f6297924bd73b008f7472634976d1
Grace Matera <gematera@iastate.edu>
2020-10-16 11:03:54 -0500

    Implementing CardView for Announcements, created new class to hold data for announcements,

---
+++ Frontend/FinalApp/app/src/main/java/com/example/studentlink/ui/home/Announcement.java
@@ -0,0 +1,12 @@
+package com.example.studentlink.ui.home;
+
+public class Announcement {
+
+    private String announcement;
+
+
+    public Announcement(String a){
+        announcement = a;
+
+    }
+}
--- Frontend/FinalApp/app/src/main/java/com/example/studentlink/ui/home/HomeFragment.java
+++ Frontend/FinalApp/app/src/main/java/com/example/studentlink/ui/home/HomeFragment.java
@@ -11,16 +11,32 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studentlink.R;
 
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    private List<Announcement> announcements;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.home_layout, container, false);
 
         TextView HomeText = root.findViewById(R.id.HomeText);
 
         HomeText.setText("Home Page Stuff");
+
+        RecyclerView rv = root.findViewById(R.id.rv);
+        //rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this.getContext());
+        rv.setLayoutManager(llm);
+
+
+
 
         return root;
     }
