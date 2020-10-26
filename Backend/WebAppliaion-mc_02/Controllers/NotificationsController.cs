5cd3a5665b0ac7ae33f8ae0949565cc82bee4b43
Greyson A. Latinwo Jones <GreysonLatinwo@gmail.com>
2020-10-25 23:14:30 -0500

    student check

---
+++ Backend/WebAppliaion-mc_02/Controllers/NotificationsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_mc_02.Models.DTO;
+
+namespace WebApplication_mc_02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        [HttpGet("{user}")]
+        public ActionResult<List<dynamic>> Get(int user)
+        {
+            return SQLConnection.get(typeof(Notifications), $"WHERE StudentID={user}");
+        }
+    }
+}
