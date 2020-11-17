78ffb2694e7ab9d7f4c0273cbe39f40007533229
Adam Brandt <abrandt1@iastate.edu>
2020-11-17 11:51:15 -0600

    tests

--- Backend/WebAppliaion-mc_02/Controllers/ChatsController.cs
+++ Backend/WebAppliaion-mc_02/Controllers/ChatsController.cs
@@ -84,7 +84,7 @@ namespace WebApplication_mc_02.Controllers
         [HttpPost]
         public async Task<ActionResult<Chats>> PostChat([FromBody] Chats chat)
         {
-            if (SQLConnection.Get<Chats>($"WHERE ChatID={chat.ChatID} and StudentID={chat.SenderID}").Result.Count < 1)
+            if (SQLConnection.Get<Chats>($"WHERE ChatID={chat.ChatID} and SenderID={chat.SenderID}").Result.Count < 1)
                 return BadRequest("your not allowed to send data in this chat");
             if (await SQLConnection.Insert<Chats>(chat))
                 return Ok(chat);
